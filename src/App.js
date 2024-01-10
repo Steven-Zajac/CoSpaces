c2a6d1493a74154f1aa0f294186d972bf1c31c6a
Steven <zajac.steve.26@gmail.com>
2024-01-09 19:46:14 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<h1>Hello World</h1>} />
+            </Routes>
+        </Router>
+    )
+
+}
+
+export default App;
