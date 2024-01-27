0ddc730902bf67a2a2882cde965ec8cf48f15ea2
Steven <zajac.steve.26@gmail.com>
2024-01-27 15:23:18 -0500

    typos

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const app = express();
 
 app.use(cors());
 app.use(morgan('tiny'));
-app.use(express.static('public')); //'./server/assets'
+app.use(express.static('../client/public')); //'./server/assets'
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 //app.use('/', express.static(__dirname + '/'))
