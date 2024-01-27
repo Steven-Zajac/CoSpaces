2e581610f3d286f0c3f48cbadbc670983b664536
Steven <zajac.steve.26@gmail.com>
2024-01-27 15:30:03 -0500

    updates

--- server/index.js
+++ server/index.js
@@ -25,10 +25,10 @@ const app = express();
 
 app.use(cors());
 app.use(morgan('tiny'));
-app.use(express.static('../client/public')); //'./server/assets'
+app.use(express.static('./server/assets')); // Is this needed?
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-//app.use('/', express.static(__dirname + '/'))
+app.use('/', express.static(__dirname + '/'))
 
 // ADD ENDPOINTS HERE //
 
